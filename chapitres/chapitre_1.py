229d7ed1bfaa78e1269935d792c4c4dd25ba7737
Aniiiiss <anisino.aek@gmail.com>
2025-12-11 13:59:46 +0100

    Fin du 3.1.2 ( input_utils.py )

---
+++ chapitres/chapitre_1.py
@@ -0,0 +1,6 @@
+from input_utils import demander_texte, demander_nombre
+from personnage import initialiser_personnage, afficher_personnage
+
+def introduction():
+    print("====== Chapitre 1 : L'arrivée dans le monde magique ======")
+    print("")
