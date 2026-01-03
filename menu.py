07c9cfd8d84895f48ac1271d44aa13614f1db727
Aniiiiss <anisino.aek@gmail.com>
2026-01-03 19:29:21 +0100

    Ajout du chapitre 5 dans le menu

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ from chapitres.chapitre_1 import lancer_chapitre_1
 from chapitres.chapitre_2 import lancer_chapitre_2
 from chapitres.chapitre_3 import lancer_chapitre_3
 from chapitres.chapitre_4 import lancer_chapitre4_quidditch
+from chapitres.chapitre_5 import lancer_chapitre_5
 
 
 def afficher_menu():
@@ -12,7 +13,6 @@ def afficher_menu():
 
 
 def lancer_menu():
-    # Dictionnaire des points des maisons pour toute l'aventure
     maisons = {
         "Gryffondor": 0,
         "Serpentard": 0,
@@ -26,17 +26,20 @@ def lancer_menu():
         choix = demander_nombre("Votre choix : ", 1, 2)
 
         if choix == 1:
-            # Chapitre 1 : création du personnage + achats
+            # Chapitre 1 : création du personnage
             personnage = lancer_chapitre_1()
 
-            # Chapitre 2 : voyage + répartition dans une maison
-            lancer_chapitre_2(personnage)
+            # Chapitre 2 : voyage + répartition ( garde le retour + passe maisons si votre fonction le demande)
+            personnage = lancer_chapitre_2(personnage, maisons)
 
-            # Chapitre 3 : apprentissage des sorts + quiz
+            # Chapitre 3 : apprentissage des sorts + quiz (met à jour maisons)
             lancer_chapitre_3(personnage, maisons)
 
-            # Chapitre 4 : match de Quidditch (scénario guidé)
+            # Chapitre 4 : Quidditch / fin d'année
             lancer_chapitre4_quidditch(personnage, maisons)
+
+            #  Chapitre 5 : boutique (extension)
+            personnage = lancer_chapitre_5(personnage, maisons)
 
         elif choix == 2:
             print("Merci d'avoir joué à Poudlard, à bientôt jeune sorcier !")
